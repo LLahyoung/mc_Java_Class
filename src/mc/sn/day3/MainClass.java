091bf8c199db06759afe0a78e832c8c7af7af01d
LLahyoung <ahyoungku@likelion.org>
2021-04-12 10:50:25 +0900

    This is mc week1 practice.

--- src/mc/sn/day3/MainClass.java
+++ src/mc/sn/day3/MainClass.java
@@ -3,10 +3,12 @@ package mc.sn.day3;
 public class MainClass {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		ServiceExample instance = new ServiceExample();
 		instance.displayMonth();
 		instance.displayGugudan();
+		
 	}
 
 }
